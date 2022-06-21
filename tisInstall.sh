8da9956eb6d6475d7ce2f340ea0e16bbd1e61225
mozhenghua <baisui@qlangtech.com>
2022-06-21 11:59:09 +0800

    add scala2.12 profile release

--- tisInstall.sh
+++ tisInstall.sh
@@ -1,1 +1,1 @@
-mvn install -Dmaven.test.skip=true -pl hudi-flink,hudi-common,packaging/hudi-flink-bundle
+mvn install -Dmaven.test.skip=true -pl hudi-flink,hudi-common,packaging/hudi-flink-bundle,packaging/hudi-utilities-bundle -Pscala-2.12
