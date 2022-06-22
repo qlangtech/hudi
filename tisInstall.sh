b4dc030a40a2026053bd0181cf955d46c0600cf9
mozhenghua <baisui@qlangtech.com>
2022-06-22 09:00:56 +0800

    add scala2.12 profile release

--- tisInstall.sh
+++ tisInstall.sh
@@ -1,1 +1,4 @@
-mvn install -Dmaven.test.skip=true -pl hudi-flink,hudi-common,packaging/hudi-flink-bundle,packaging/hudi-utilities-bundle -Pscala-2.12
+# for spark3
+#mvn install -Pspark3,flink-bundle-shade-hive3 -Dmaven.test.skip=true -pl hudi-flink,hudi-common,packaging/hudi-flink-bundle,packaging/hudi-utilities-bundle
+
+mvn deploy -Pspark3,flink-bundle-shade-hive3 -Dmaven.test.skip=true -pl hudi-flink,hudi-common,packaging/hudi-flink-bundle,packaging/hudi-utilities-bundle -DaltDeploymentRepository=base::default::http://localhost:8080/release
