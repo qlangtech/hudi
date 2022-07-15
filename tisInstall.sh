3124c05005dc1205db7b4e464f7af0f218b403ac
mozhenghua <baisui@qlangtech.com>
2022-07-15 14:13:24 +0800

    small fix

--- tisInstall.sh
+++ tisInstall.sh
@@ -5,9 +5,9 @@
 
 #MVN_PROFILE="spark3,flink-bundle-shade-hive3"
 compileAndDeploy(){
-MVN_PROFILE = $1
+MVN_PROFILE="$1"
 echo "compile and deploy with maven profile:$MVN_PROFILE "
-mvn deploy -P$MVN_PROFILE \
+mvn clean deploy -P$MVN_PROFILE \
  -Dmaven.test.skip=true \
  -pl hudi-flink,hudi-common\
 ,packaging/hudi-flink-bundle\
